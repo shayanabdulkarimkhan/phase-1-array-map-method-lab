7fb7d51e2c8feabc1ea7ff598ee040e6e60b152c
shayan <sak704@nyu.edu>
2022-02-23 19:41:09 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -12,5 +12,55 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  const titleTut = tutorials.map(
+
+    function (arr) {
+      let newArr;      
+      for (let i=0; i<arr.length; i++){
+        
+        if (i=== 0) {
+          newArr = arr[0].charAt(0).toUpperCase();
+        } else if (arr[i-1] === " "){
+          newArr = `${newArr}${arr[i].toUpperCase()}`;
+        } else {
+          newArr = `${newArr}${arr[i]}`;
+        }
+      }
+      
+      return newArr;
+      // const chArr = newArr.map(function (ele,index,arr) {
+      //   if (index >0) {
+      //     if (newArr[index-1] === " ") {
+      //       console.log(ele.toUpperCase());
+      //     } else {
+      //       return ele;
+      //     }
+          
+      //   } else {
+      //     return arr[0].charAt(0).toUpperCase(); 
+      //   }
+      // });
+      
+      // console.log(chArr);
+
+      // newArr = arr.map(function(ele){
+      //   i++;
+      //   if (ele === " "){
+      //     return arr[i+1].toUpperCase();
+      //   }        
+      // })
+
+      
+      
+    }     
+     
+  )
+  return titleTut;
 }
+
+console.log(titleCased());
+
+
+
+
